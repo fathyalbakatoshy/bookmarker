dd0c2e5296ceeb3a25759b7007c08e34e3e39d2d
Fathi Mostafa <tetomostafa20141@gmail.com>
2023-12-17 11:23:38 -0800

    Add files via upload
    New Commit

---
+++ js/main.js
@@ -0,0 +1,85 @@
+var siteName = document.getElementById("siteName");
+var siteUrl = document.getElementById("siteUrl");
+
+var bookList = [];
+
+if (localStorage.getItem("books") == null) {
+  bookList = [];
+} else {
+  bookList = JSON.parse(localStorage.getItem("books"));
+  display(bookList);
+}
+
+function addBook() {
+  if (validateBookName() && validateBookUrl()) {
+    var book = {
+      name: siteName.value,
+      url: siteUrl.value,
+    };
+
+    bookList.push(book);
+    localStorage.setItem("books", JSON.stringify(bookList));
+    display(bookList);
+    clearInput();
+    siteName.classList.remove("is-valid")
+    siteUrl.classList.remove("is-valid")
+  }
+}
+
+function clearInput() {
+  siteName.value = "",
+  siteUrl.value = ""
+}
+
+function display(value) {
+  let box = ``;
+  for (let i = 0; i < value.length; i++) {
+    box += `
+    <tr>
+      <td>${i + 1}</td>
+      <td>${value[i].name}</td>
+      <td><button class="btn btn-danger" onclick="visitSite('https://${value[i].url}')">Visit</button></td>
+      <td><button onclick="deleteBook(${i})" class="btn btn-danger">Delete</button></td>
+    </tr>
+    `;
+  }
+  document.getElementById("display").innerHTML = box;
+}
+
+function visitSite(URL) {
+  window.open(URL);
+}
+
+function deleteBook(index) {
+  bookList.splice(index, 1);
+  localStorage.setItem("books", JSON.stringify(bookList));
+  display(bookList);
+}
+
+function validateBookName() {
+  let regex = /^[A-Z][a-z]{2,}$/;
+  if (regex.test(siteName.value)) {
+    siteName.classList.add("is-valid");
+    siteName.classList.remove("is-invalid");
+    // siteName.classList.replace("valid-feedback", "invalid-feedback");
+    return true;
+  } else {
+    siteName.classList.add("is-invalid");
+    siteName.classList.remove("is-valid");
+    return false;
+  }
+}
+
+function validateBookUrl() {
+  let regex =/^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[a-zA-Z0-9-._]*)*$/;
+  if (regex.test(siteUrl.value)) {
+    siteUrl.classList.add("is-valid");
+    siteUrl.classList.remove("is-invalid");
+    return true;
+  } else {
+    siteUrl.classList.add("is-invalid");
+    siteUrl.classList.remove("is-valid");
+    return false;
+  }
+}
+
